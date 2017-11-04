8cfa353c484f346d01db64688a5d988d23e0d9ed
Vincent <cloudy.sonata@gmail.com>
2017-11-04 17:30:29 -0400

    Initial Commit

---
+++ Frostlight Shopping ListTests/Frostlight_Shopping_ListTests.swift
@@ -0,0 +1,36 @@
+//
+//  Frostlight_Shopping_ListTests.swift
+//  Frostlight Shopping ListTests
+//
+//  Created by Vincent on 11/4/17.
+//  Copyright © 2017 Frostlight. All rights reserved.
+//
+
+import XCTest
+@testable import Frostlight_Shopping_List
+
+class Frostlight_Shopping_ListTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Frostlight Shopping List' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Frostlight Shopping List
+
+  target 'Frostlight Shopping ListTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
